25e905f7db35bc2b4c2bfd6eef7411d40a13768e
chandler <parksm0529@gmail.com>
2024-12-03 17:55:43 +0900

    feat) Find items for GET /basic/items

---
+++ src/main/java/com/chandler/springmvc1/spring/project/controller/BasicItemController.java
@@ -0,0 +1,34 @@
+package com.chandler.springmvc1.spring.project.controller;
+
+import com.chandler.springmvc1.spring.project.domain.Item;
+import com.chandler.springmvc1.spring.project.domain.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/basic/items")
+@Controller
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "items";
+    }
+
+    @PostConstruct // for test
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
