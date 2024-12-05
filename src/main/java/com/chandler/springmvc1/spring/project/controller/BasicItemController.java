2318deb40e6745ba8d3a82b80496cc789b3803b9
chandler <parksm0529@gmail.com>
2024-12-05 15:39:22 +0900

    feat) View page - item

--- src/main/java/com/chandler/springmvc1/spring/project/controller/BasicItemController.java
+++ src/main/java/com/chandler/springmvc1/spring/project/controller/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,6 +24,13 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String getItem(@PathVariable Long itemId, Model model) {
+        var item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "item";
     }
 
     @PostConstruct // for test
