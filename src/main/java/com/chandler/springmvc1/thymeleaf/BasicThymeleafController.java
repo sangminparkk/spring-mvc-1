377141c79d74d7295776b14e61c4a8a341b71918
chandler <parksm0529@gmail.com>
2024-12-06 17:08:06 +0900

    view) Print url link with query-string/path-variable:

--- src/main/java/com/chandler/springmvc1/thymeleaf/BasicThymeleafController.java
+++ src/main/java/com/chandler/springmvc1/thymeleaf/BasicThymeleafController.java
@@ -48,6 +48,13 @@ public class BasicThymeleafController {
         return "basic/basic-object";
     }
 
+    @GetMapping("/link")
+    public String link(Model model){
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
 
 
 
