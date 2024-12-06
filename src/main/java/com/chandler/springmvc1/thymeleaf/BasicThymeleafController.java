a300fb32d23b636caf1cea286645674f7ffbeba6
chandler <parksm0529@gmail.com>
2024-12-06 16:15:23 +0900

    Print variable with springEL

--- src/main/java/com/chandler/springmvc1/thymeleaf/BasicThymeleafController.java
+++ src/main/java/com/chandler/springmvc1/thymeleaf/BasicThymeleafController.java
@@ -5,6 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RequestMapping("/basic")
 @Controller
 public class BasicThymeleafController {
@@ -14,5 +19,28 @@ public class BasicThymeleafController {
         model.addAttribute("data", "Hello <b>Spring</b>");
         return "basic/text-basic";
     }
+
+    @GetMapping("/springEL")
+    public String springEL(Model model){
+
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> users = new ArrayList<>();
+        users.add(userA);
+        users.add(userB);
+
+        Map<String, User> userMap = new HashMap<>();
+        userMap.put("userA", userA);
+        userMap.put("userB", userB);
+
+        model.addAttribute("userA", userA);
+        model.addAttribute("users", users);
+        model.addAttribute("userMap", userMap);
+
+        return "basic/springEL";
+    }
+
+
 
 }
