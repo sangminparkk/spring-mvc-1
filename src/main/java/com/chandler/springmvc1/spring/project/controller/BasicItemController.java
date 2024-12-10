a9bf2195284930dcdb6f52270cda90e6b01e6726
chandler <parksm0529@gmail.com>
2024-12-10 11:59:28 +0900

    view) Select box by java class

--- src/main/java/com/chandler/springmvc1/spring/project/controller/BasicItemController.java
+++ src/main/java/com/chandler/springmvc1/spring/project/controller/BasicItemController.java
@@ -1,5 +1,6 @@
 package com.chandler.springmvc1.spring.project.controller;
 
+import com.chandler.springmvc1.spring.project.domain.DeliveryCode;
 import com.chandler.springmvc1.spring.project.domain.Item;
 import com.chandler.springmvc1.spring.project.domain.ItemRepository;
 import com.chandler.springmvc1.spring.project.domain.ItemType;
@@ -11,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,6 +37,15 @@ public class BasicItemController {
     @ModelAttribute("itemTypes")
     public ItemType[] itemTypes() {
         return ItemType.values();
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+        return deliveryCodes;
     }
 
     @GetMapping
