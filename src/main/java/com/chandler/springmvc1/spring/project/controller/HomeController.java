5580bfa8a712254ee9a8fbc81116900a3f2c823d
chandler <parksm0529@gmail.com>
2024-12-16 23:55:31 +0900

    feat) Logout by expired cookie

--- src/main/java/com/chandler/springmvc1/spring/project/controller/HomeController.java
+++ src/main/java/com/chandler/springmvc1/spring/project/controller/HomeController.java
@@ -2,11 +2,14 @@ package com.chandler.springmvc1.spring.project.controller;
 
 import com.chandler.springmvc1.spring.project.domain.Member;
 import com.chandler.springmvc1.spring.project.domain.MemberRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -34,5 +37,16 @@ public class HomeController {
         return "loginHome";
     }
 
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expiredCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private static void expiredCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
 
 }
