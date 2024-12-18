2075cfb60919205d5579c460d31827a4ad01afba
chandler <parksm0529@gmail.com>
2024-12-18 22:58:55 +0900

    feat) Register fiter as bean on configuration

---
+++ src/main/java/com/chandler/springmvc1/spring/project/config/WebConfig.java
@@ -0,0 +1,33 @@
+package com.chandler.springmvc1.spring.project.config;
+
+import com.chandler.springmvc1.spring.project.web.LogFilter;
+import com.chandler.springmvc1.spring.project.web.LoginCheckFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+
+        return filterFilterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterFilterRegistrationBean.setOrder(2);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+
+        return filterFilterRegistrationBean;
+    }
+
+}
