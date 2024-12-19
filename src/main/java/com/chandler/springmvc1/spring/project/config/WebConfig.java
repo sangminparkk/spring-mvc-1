43ffcb94bee754a129d3d0cb68cb725020ba1022
chandler <parksm0529@gmail.com>
2024-12-19 21:58:31 +0900

    refactor) @Login by argumentResolver

--- src/main/java/com/chandler/springmvc1/spring/project/config/WebConfig.java
+++ src/main/java/com/chandler/springmvc1/spring/project/config/WebConfig.java
@@ -1,17 +1,22 @@
 package com.chandler.springmvc1.spring.project.config;
 
-import com.chandler.springmvc1.spring.project.web.LogFilter;
-import com.chandler.springmvc1.spring.project.web.LogInterceptor;
-import com.chandler.springmvc1.spring.project.web.LoginCheckFilter;
-import com.chandler.springmvc1.spring.project.web.LoginCheckInterceptor;
+import com.chandler.springmvc1.spring.project.web.*;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
--- src/main/java/com/chandler/springmvc1/spring/project/controller/HomeController.java
+++ src/main/java/com/chandler/springmvc1/spring/project/controller/HomeController.java
@@ -1,14 +1,13 @@
 package com.chandler.springmvc1.spring.project.controller;
 
+import com.chandler.springmvc1.spring.project.web.Login;
 import com.chandler.springmvc1.spring.project.domain.Member;
 import com.chandler.springmvc1.spring.project.domain.MemberRepository;
-import com.chandler.springmvc1.spring.project.web.SessionConst;
 import com.chandler.springmvc1.spring.project.web.SessionManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,7 +22,7 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String homeLogin(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+    public String homeLogin(@Login Member loginMember, Model model) {
         if (loginMember == null) {
             return "home";
         }
