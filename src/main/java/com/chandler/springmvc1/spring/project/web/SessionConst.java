696849180e0f50b3a9ebd1d309f1d09f53e4ef65
chandler <parksm0529@gmail.com>
2024-12-20 12:55:59 +0900

    polishing to work

--- src/main/java/com/chandler/springmvc1/spring/project/web/SessionConst.java
+++
@@ -1,7 +0,0 @@
-package com.chandler.springmvc1.spring.project.web;
-
-public interface SessionConst {
-
-    String LOGIN_MEMBER = "loginMember";
-
-}
--- src/main/java/com/chandler/springmvc1/web/frontcontroller/v4/ControllerV4.java
+++
@@ -1,8 +0,0 @@
-package com.chandler.springmvc1.web.frontcontroller.v4;
-
-import java.util.Map;
-
-public interface ControllerV4 {
-
-    String process(Map<String, String> paramMap, Map<String, Object> model);
-}
