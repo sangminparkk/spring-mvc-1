5a3249964f449a812e5cd044118448bf4e8803c3
chandler <parksm0529@gmail.com>
2024-12-21 17:47:53 +0900

    feat) How to work an exception by interceptor

--- src/main/java/com/chandler/springmvc1/servlet/config/WebConfig.java
+++ src/main/java/com/chandler/springmvc1/servlet/config/WebConfig.java
@@ -1,18 +1,28 @@
 package com.chandler.springmvc1.servlet.config;
 
 import com.chandler.springmvc1.servlet.filter.LogFilter;
+import com.chandler.springmvc1.servlet.filter.LogInterceptor;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static jakarta.servlet.DispatcherType.ERROR;
 import static jakarta.servlet.DispatcherType.REQUEST;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/error", "/*.ico", "/error-page/**");
+    }
+
+//    @Bean
     public FilterRegistrationBean filterRegistrationBean() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
